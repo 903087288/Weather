6db983ec673056d15ee68e6c14de52fe01a6ba59
刘超 <1140053683@qq.com>
2017-05-08 14:03:30 +0800

    注释

--- app/src/main/java/com/example/tianqiyubao/MainActivity.java
+++ app/src/main/java/com/example/tianqiyubao/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//主函数
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
